07f95bbe1b286fe14853171717bd4a17d1c9b11c
Renu kumari <renuk1485@gmail.com>
2022-03-09 21:06:36 +0530

    install app

---
+++ unit-5/sprint-2/day-3/assignments/timer/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+     <h4>timer using useEffect2</h4>
+     <hr/>
+    </div>
+  )
+}
+
+export default App
