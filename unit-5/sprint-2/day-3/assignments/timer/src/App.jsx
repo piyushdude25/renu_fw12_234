fbaf1d10ac9ac1f2e723d8538449836cdacb00fc
Renu kumari <renuk1485@gmail.com>
2022-03-13 09:49:49 +0530

    comit

--- unit-5/sprint-2/day-3/assignments/timer/src/App.jsx
+++ unit-5/sprint-2/day-3/assignments/timer/src/App.jsx
@@ -2,15 +2,16 @@ import { useState } from 'react'
 import { Timer } from './components/Timer';
 import './App.css'
 
+
 function App() {
-  const [start, setStart] = useState(0);
-  const [end, setEnd] = useState(50);
+  
+  
 
   return (
     <div className="App">
      <h4>timer using useEffect2</h4>
      <hr/>
-     <Timer start={start} end={end}/>
+     <Timer start="0" end="6"/>
     </div>
   )
 }
--- unit-5/sprint-2/day-3/assignments/timer/src/components/Timer.jsx
+++ unit-5/sprint-2/day-3/assignments/timer/src/components/Timer.jsx
@@ -1,4 +1,28 @@
+import { useEffect } from "react";
+import { useRef } from "react";
+import { useState } from "react";
+
 export const Timer=(props)=>{
-    return <div>Timer:{props.start}</div>
+   const ref=useRef(null);
+    console.log(props.start)
+    const[start,setStart]=useState(+props.start);
+    const[end,setEnd]=useState(+props.end);
+    console.log("current",ref.current)
+    useEffect(()=>{
+    ref.current=    setInterval(()=>{
+            setStart((p)=>{
+                
+              return  (p==end?clearInterval(ref.current):p+1);
+            })
+        },1000)
+
+    },[])
+   
+   
+   
+   
+    return <div><h4>Timer:{start}</h4>
+
+    </div>
     
 }
