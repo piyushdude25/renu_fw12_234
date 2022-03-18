df47c222344e7fd47708ba991a24a331f17f3398
Renu kumari <renuk1485@gmail.com>
2022-03-18 23:07:26 +0530

    assigmet

---
+++ unit-5/sprint-3/day-1/assignments/context/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+     <h4>Context api assignments...............</h4>
+    </div>
+  )
+}
+
+export default App
