2ace11d7a1c833672e93860ce7cdf299c4f55243
Renu kumari <renuk1485@gmail.com>
2022-04-10 20:16:29 +0530

    Update AuthContext.jsx

--- unit-5/sprint-3/day-1/assignments/context/src/contexts/AuthContext.jsx
+++ unit-5/sprint-3/day-1/assignments/context/src/contexts/AuthContext.jsx
@@ -1,8 +1,8 @@
 import { createContext } from "react";
 import { useState } from "react";
-const AuthContext= createContext();
-export const AuthContextProvider=({children})=>{
-    const[isAuth,setisAuth]=useState(false);
-    const[toggleAuth,settoggleAuth]=useState(false)
-    return <AuthContext.provider >{children}</AuthContext.provider>
-}
+export const AuthContext = createContext();
+export const AuthContextProvider = ({ children }) => {
+  const [isAuth, setisAuth] = useState(false);
+  const [toggleAuth, settoggleAuth] = useState(false);
+  return <AuthContext.provider>{children}</AuthContext.provider>;
+};
